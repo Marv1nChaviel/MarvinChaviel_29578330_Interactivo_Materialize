e9bca2b0b8ec278f5cf88e4c30b7d27583d5b1c3
Marv1nChaviel <100033251+Marv1nChaviel@users.noreply.github.com>
2022-04-07 22:38:01 -0400

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,20 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, options);
+});
+
+// Or with jQuery
+
+
+//SLider 
+$(document).ready(function(){
+  $('.slider').slider();
+});
